2e0c6bdb6aaeb1de249ff513a113bd62bfc13985
eleanorstrib <hello@eleanorstrib.com>
2017-10-24 21:29:39 -0700

    added function to detect sexist words, first cut

--- analysis.py
+++ analysis.py
@@ -1,9 +1,13 @@
 from nltk import pos_tag, sent_tokenize, Text, word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from words import sexist_words as w
 
 # determine who the dialog is spoken by (character name/gender)
 # check if there is an adjective associated with their speech
 # put the adjective into a dict, m/f, count instances of the adjective
 
+l = WordNetLemmatizer()
 
 def tokenize_text(textfile):
     """
@@ -19,27 +23,35 @@ def tokenize_text(textfile):
     for sent in s_tokens:
         s = word_tokenize(sent)
         full_text.append(s)
-
-
     return full_text
 
-def classify_words(full_text):
+def classify_words(sentence):
     """
     Applies NLTK's word classifier to identify parts of speech and returns a list
     of sentences, each a list of tuples where the tuples are classified words.
     """
-    classified_list = []
-    for sent in full_text:
-        # list of tuples that look like this ('summer', 'NN'), ("''", "''")
-        tagged_text = pos_tag(sent)
-        classified_list.append(tagged_text)
-    return classified_list
+    tagged_text = pos_tag(sentence)
+    return tagged_text
 
-def find_character(classified_list, character):
+
+def find_character(w, classified_list, character):
+    s_words = list(w.keys())
+    count = 0
     for s in classified_list:
-        if 'bossy' in s:
-            print(s)
+        word_in_s = any(w in s for w in s_words)
+        if word_in_s == True:
+            count+=1
+    print(count)
+    print(len(classified_list))
+    #     for word in s_words:
+    #         if word in s:
+    #             found_word = word
+    #             found_index = s.index(word)
+    #             print(s)
+
+        #     print(c)
+
 
 
 full_text = tokenize_text('corpus/hp1.txt')
-find_character(full_text, 'Hermione')
+find_character(w, full_text, 'Hermione')
